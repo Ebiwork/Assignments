b320a34843a20f0d2c12d4d7d69c8cf86e26cf67
Ebiwork <jack.ibiso609@gmail.com>
2022-10-26 12:54:49 -0700

    first commit

---
+++ week3/portfolio/tesla/script.js
@@ -0,0 +1,11 @@
+var house = {
+    rome: "bed",
+    parlor: "chair",
+    kitchen: "gas",
+    pavment: "flower vest",
+    secondhouse: {
+        smallroom: "fan",
+        smallroomb: "window",
+       smallroomc: "carpet",
+    }  
+}
